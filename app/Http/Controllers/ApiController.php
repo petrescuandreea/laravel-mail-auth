79de7b9aa1f9cb1bd38da2c618cff522d8a68817
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-14 21:37:32 +0100

    Create sendDeleteMail method in ApiController

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 
 use App\Videogame;
+use App\Mail\VideogameDeleteMail;
 
 class ApiController extends Controller
 {
@@ -18,5 +21,18 @@ class ApiController extends Controller
 
         $videogame = Videogame::findOrFail($id);
         $videogame -> delete();
+        
+        // invio mail 
+        $this -> sendDeleteMail($videogame);
+
+        return json_encode($videogame);
+    }
+
+    private function sendDeleteMail($videogame) {
+
+        // mail inviata all'utente registrato
+        Mail::to(Auth::user() -> email) -> send(new VideogameDeleteMail($videogame));
+        // mail inviata all'amministratore
+        Mail::to('admin@miosito.com') -> send(new VideogameDeleteMail($videogame));
     }
 }
