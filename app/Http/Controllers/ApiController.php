e8938c9b52372a33504f89b16594b0685e534357
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-14 15:58:11 +0100

    Create ApiController and add getVideogames method

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Videogame;
+
+class ApiController extends Controller
+{
+    public function getVideogames() {
+
+        $videogames = Videogame::all();
+        return json_encode($videogames);
+    }
+}
