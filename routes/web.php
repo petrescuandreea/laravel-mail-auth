8e2670eec6e00f3a88d969b2b0cda5c9fc8e0576
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-14 16:27:27 +0100

    Create register, login and logout routes

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'GuestController@home') -> name('home');
 
+Route::post('/register', 'Auth\RegisterController@register') -> name('register');
+Route::post('/login', 'Auth\LoginController@login') -> name('login');
+Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
+
 Route::get('/api/videogames/index', 'ApiController@getVideogames') -> name('api.videogames.index');
