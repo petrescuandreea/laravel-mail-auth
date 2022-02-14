dcd0b840763fb2db49b48197561cb6efc8b93861
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-14 16:31:40 +0100

    Add register and login forms in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,50 @@
 @extends('layouts.main-layout')
 @section('content')
-    <h2>
-        Videogames
-    </h2>
+    @auth
+        <h1>
+            Hello {{ Auth::user() -> name }}
+        </h1>
 
+        <a class="btn btn-danger" href="{{ route('logout') }}">LOGOUT</a>
+    @else
+        {{-- login form  --}}
+        <h2>Login</h2>
+        <form action="{{ route('login') }}" method="POST">
+            @method("POST")
+            @csrf
+
+            <label for="email">Email</label><br>
+            <input type="email" name="email"><br>
+
+            <label for="password">Password</label><br>
+            <input type="password" name="password"><br>
+            <br>
+
+            <button class="btn btn-primary" type="submit" > Login</button>
+        </form>
+
+        {{-- registration form  --}}
+        <h2>Registration</h2>
+        <form action="{{ route('register') }}" method="POST">
+            @method("POST")
+            @csrf
+
+            <label for="name">Name</label><br>
+            <input type="text" name="name"><br>
+
+            <label for="email">Email</label><br>
+            <input type="email" name="email"><br>
+
+            <label for="password">Password</label><br>
+            <input type="password" name="password"><br>
+
+            <label for="password_confirmation">Password Confirmation</label><br>
+            <input type="password" name="password_confirmation"><br>
+            <br>
+
+            <button class="btn btn-primary" type="submit" > Register</button>
+        </form>
+    @endauth
+    
     <videogames-component></videogames-component>
 @endsection
