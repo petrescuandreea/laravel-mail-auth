c71ed438284ef43c8aa9358ee6b4faceed0648ed
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-14 15:10:15 +0100

    Create main-layout

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <script src="{{ asset('/js/app.js') }}"></script>
+    <title>Videogames</title>
+</head>
+<body>
+
+    <div id="app">
+        @include('components.header')
+
+        @yield('content')
+
+        @include('components.footer')
+    </div>
+    
+</body>
+</html>
